1234bb0dd7ab4ae0e13822b6622b0af7428d8a49
LS <email@xxxbla.com>
2023-09-22 12:21:45 -0600

    Se completa el home

--- resources/js/components/page/home/index.vue
+++ resources/js/components/page/home/index.vue
@@ -3,7 +3,7 @@
 
     <section class="suggestion-section">
       <div class="box-text">
-        <h2 class="title-h1">Si, talvez<br />necesitas lentes</h2>
+        <h2 class="title-h1">Si, talvez<br />necesitas lentes.</h2>
         <h5 class="txt-2">
           Si llegaste hasta aquí es porque tienes esa pequeña duda.<br />
           Pero, tranquilo, es fácil resolverla con un sencillo test.
@@ -13,10 +13,10 @@
           <router-link class="t-150 btn-s2" to="/test">Hacer test</router-link>
         </p>
 
-        <h6 class="txt-3">
+        <router-link class="txt-3" to="/obtener-lentes">
           Ya se que los necesito.<br />
           Solo quiero aprovechar la oferta.
-        </h6>
+        </router-link>
       </div>
 
       <div class="box-image" v-bind:style="{ backgroundImage: 'url(public/images/pages/home/h-glasses-1.jpg)' }">
@@ -44,51 +44,12 @@
 
         <div class="box-faqs">
           <div class="accordion" role="tablist">
-            <b-card no-body>
+            <b-card no-body v-for="(item, qaInx) in faqs" :key="'qaInx'+qaInx">
               <b-card-header header-tag="header" role="tab">
-                <a class="btn-faq-toggle" v-b-toggle.accordion-1>Pregunta 1</a>
+                <a class="btn-faq-toggle" v-b-toggle="'accordion-'+qaInx">{{ item.q }}</a>
               </b-card-header>
 
-              <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
-                <b-card-body>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-                </b-card-body>
-              </b-collapse>
-            </b-card>
-
-            <b-card no-body>
-              <b-card-header header-tag="header" role="tab">
-                <a class="btn-faq-toggle" v-b-toggle.accordion-2>Pregunta 2</a>
-              </b-card-header>
-              <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-                <b-card-body>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-                </b-card-body>
-              </b-collapse>
-            </b-card>
-
-            <b-card no-body>
-              <b-card-header header-tag="header" role="tab">
-                <a class="btn-faq-toggle" v-b-toggle.accordion-3>Pregunta 3</a>
-              </b-card-header>
-              <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-                <b-card-body>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-                </b-card-body>
-              </b-collapse>
-            </b-card>
-
-            <b-card no-body>
-              <b-card-header header-tag="header" role="tab">
-                <a class="btn-faq-toggle" v-b-toggle.accordion-4>Pregunta 3</a>
-              </b-card-header>
-              <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+              <b-collapse :id="'accordion-'+qaInx" accordion="my-accordion" role="tabpanel">
                 <b-card-body>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -158,6 +119,13 @@
 export default {
   data() {
     return {
+      faqs: [
+        { q: 'Pregunta 1' },
+        { q: 'Pregunta 2' },
+        { q: 'Pregunta 3' },
+        { q: 'Pregunta 4' },
+      ],
+
       form: {
         name: null,
         phone: null,
