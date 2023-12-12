5c7391ff2c3aaf0b47e1b8e456a42129080ab2d3
mmal73 <luisito250899@gmail.com>
2023-12-11 22:06:38 -0600

    feat: browser-sync added to watch vue files

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,3 +19,16 @@ mix.sass('resources/sass/app.scss', 'public/css').sourceMaps();
 
 mix.js('resources/js/app.js', 'public/js').vue();
 mix.js('resources/js/admin.js', 'public/js').vue();
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+if (!mix.inProduction()) {
+    mix.browserSync({
+        proxy: process.env.MIX_APP_URL,
+        ui: {
+            port: 8080
+        }
+    });
+}
